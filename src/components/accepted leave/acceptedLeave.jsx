c8d8dfd5faba2a5bec0e8c1bb78fe60734e08836
simeontambu <simeontambu@gmail.com>
2024-02-28 17:03:47 +0100

    adding accept leave components

--- src/components/accepted leave/acceptedLeave.jsx
+++ src/components/accepted leave/acceptedLeave.jsx
@@ -1,8 +1,37 @@
+import { useData } from "../../hooks/useData";
+
 export default function AcceptedLeave() {
-    return (
-      <>
-        <div>AcceptedLeave</div>
-      </>
-    );
-  }
-  
+  const { planning } = useData();
+  return (
+    <>
+      <table className="table-auto w-[50.25em] bg-[#9dc3cc] my-auto mx-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2 ">Code congé</th>
+            <th className="px-4 py-2">Nom agent</th>
+            <th className="px-4 py-2">Date départ</th>
+            <th className="px-4 py-2">Date retour</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {planning.map((plannings) => (
+            <tr key={plannings.id}>
+              <td className="px-4 py-2 border border-r-gray-400">
+                {plannings.code_conge}
+              </td>
+              <td className="px-4 py-2 border border-r-gray-400">
+                {plannings.matricule_agent}
+              </td>
+              <td className="px-4 py-2 border border-r-gray-400">
+                {plannings.date_depart}
+              </td>
+              <td className="px-4 py-2 border border-r-gray-400">
+                {plannings.date_retour}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
